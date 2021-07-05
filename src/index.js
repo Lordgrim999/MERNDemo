ade2fafc6a65fd03444f6ce627f5890db91fcd1a
Lordgrim999 <srivastavaak47rishi@gmail.com>
2021-07-05 22:14:54 +0530

    All files added

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const App = (props) => {
+  return <h2 className="text-center">{props.headerMessage}</h2>;
+};
+
+App.propTypes = {
+  headerMessage: PropTypes.string,
+};
+
+App.defaultProps = {
+  headerMessage: "Hello!!",
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
